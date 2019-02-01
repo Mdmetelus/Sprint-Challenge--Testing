e1599fad2adf4b462087d6f864dc7a7dd170d0c8
Mr. Metelus <Mdmetelus@gmail.com>
2019-02-01 12:52:04 -0500

    Post endpoint routes added

--- index.spec.js
+++ index.spec.js
@@ -11,6 +11,7 @@ describe('The APIs Endpoints', function() {
             let response = await request(server).get('/games');
             expect(response.status).toBe(200);
         });
+
         it('should always return an array type even if an empty array', async () => {
             const response = await request(server).get('/games');
             expect(Array.isArray(response.body)).toBeTruthy();
@@ -27,5 +28,40 @@ describe('The APIs Endpoints', function() {
         });
 
     });
+
+
+    describe('POST /games route', () => {
+
+        it('should return status code of 201 when working correctly', async () => {
+            let response = await request(server)
+                .post('/games')
+                .send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+            expect(response.status).toBe(201);
+        });
+
+
+        it('should return message if the game was succesfully added', async () => {
+            let response = await request(server)
+                .post('/games')
+                .send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+            expect(response.body).toEqual({ message: 'New game added' });
+        });
+
+        it('should return status code of 422 without title or genre included', async () => {
+            let response = await request(server)
+                .post('/games')
+                .send({});
+            expect(response.status).toBe(422);
+        });
+
+        it('should return message if the game was not added', async () => {
+            let response = await request(server)
+                .post('/games')
+                .send({ title: 'Pacman', releaseYear: 1980 });
+            expect(response.body).toEqual({ Error: 'Please add the title and genre' });
+        });
+
+    });
+
 
 });
