3c9ce2327fd75d5ed5a33753ae0de5619295d66a
Mr. Metelus <Mdmetelus@gmail.com>
2019-02-01 13:11:22 -0500

    git route made

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const server = express();
 
+const db = require('./data/dbConfig');
+
+
 server.use(express.json());
 
+
+server.get('/', (req, res) => {
+
+    res.status(200).send(`API working.\n Sanity Check\n Test Route!`);
+
+});
+
+server.get('/games', (req, res) => {
+    db('games')
+    .then(games => res.status(200).json(games))
+    .catch(error =>res.status(500).json(error))
+});
 
 module.exports = server;
